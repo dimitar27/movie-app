6b6533f4aa1e32135de188869aab6a09b085b8b1
Dimitar <dimi.dimitrow27@gmail.com>
2025-03-09 20:52:27 +0100

    Create storage classes

---
+++ movie_storage.py
@@ -0,0 +1,59 @@
+import json
+
+
+def get_movies():
+    """
+    Returns a dictionary of dictionaries that
+    contains the movies information in the database.
+
+    The function loads the information from the JSON
+    file and returns the data.
+    """
+    with open("data.json", "r") as fileobj:
+        return json.loads(fileobj.read())
+
+
+def save_movies(movies):
+    """
+    Gets all your movies as an argument and saves them to the JSON file.
+    """
+    json_str = json.dumps(movies)
+    with open("data.json", "w") as fileobj:
+        fileobj.write(json_str)
+
+
+def add_movie(title, rating, year):
+    """
+    Adds a movie to the movies database.
+    Loads the information from the JSON file, add the movie,
+    and saves it. The function doesn't need to validate the input.
+    """
+
+    movies = get_movies()
+    new_movie = {title: {"rating": rating, "year": year}}
+    movies.update(new_movie)
+    save_movies(movies)
+
+
+def delete_movie(title):
+    """
+    Deletes a movie from the movies database.
+    Loads the information from the JSON file, deletes the movie,
+    and saves it. The function doesn't need to validate the input.
+    """
+
+    movies = get_movies()
+    del movies[title]
+    save_movies(movies)
+
+
+def update_movie(title, rating):
+    """
+    Updates a movie from the movies database.
+    Loads the information from the JSON file, updates the movie,
+    and saves it. The function doesn't need to validate the input.
+    """
+
+    movies = get_movies()
+    movies[title]["rating"] = rating
+    save_movies(movies)
---
+++ storage_json.py
@@ -0,0 +1,94 @@
+from istorage import IStorage
+import json
+
+
+class StorageJson(IStorage):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+
+    def list_movies(self):
+        """Retrieves the list of movies from the
+        JSON file and returns the data."""
+        try:
+            with open(self.file_path, "r") as fileobj:
+                data = json.load(fileobj)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = {}
+        return data
+
+
+    def add_movie(self, title, year: int, rating: int, poster=None):
+        """Adds a movie to the movies database.
+        Loads the information from the JSON file, add the movie,
+        and saves it."""
+        try:
+            year = int(year)
+            rating = float(rating)
+        except ValueError:
+            print("Please enter a valid rating (numbers only)!")
+            return
+
+        try:
+            with open(self.file_path, 'r') as fileobj:
+                data = json.load(fileobj)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = {}
+
+        if title in data:
+            print(f"Movie {title} already exists!")
+        elif year < 1900 or year > 2025:
+            print(f"Please enter a valid year between 1900 and 2025.")
+        elif rating < 0 or rating > 10:
+            print(f"Rating must be between 0 and 10!")
+        else:
+            new_move = {title: {"rating": rating, "year": year}}
+            data.update(new_move)
+
+            with open(self.file_path, 'w') as fileobj:
+                json.dump(data, fileobj, indent=4)
+
+
+
+    def delete_movie(self, title):
+        """Deletes a movie from the movies database.
+        Loads the information from the JSON file, deletes the movie,
+        and saves it."""
+        try:
+            with open(self.file_path, 'r') as fileobj:
+                data = json.load(fileobj)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = {}
+
+        if title in data:
+            del data[title]
+            with open(self.file_path, 'w') as fileobj:
+                json.dump(data, fileobj, indent=4)
+            print(f"Movie {title} successfully deleted")
+        else:
+            print(f"Movie {title} doesn't exist!")
+
+
+
+    def update_movie(self, title, rating):
+        """Updates a movie from the movies database."""
+        try:
+            with open(self.file_path, 'r') as fileobj:
+                data = json.load(fileobj)
+        except (FileNotFoundError, json.JSONDecodeError):
+            data = {}
+
+        if title not in data:
+            print(f"Movie {title} doesn't exist!")
+        elif 0 <= rating <= 10:
+            data[title]["rating"] = rating
+            with open(self.file_path, 'w') as fileobj:
+                json.dump(data, fileobj, indent=4)
+        else:
+            print("Rating must be between 0 and 10!")
+
+
+
+movie = StorageJson('data.json')
+movie.add_movie("Lord Of The Rings", 2000, 10)
+movie.update_movie("Harry Potter", 0)
